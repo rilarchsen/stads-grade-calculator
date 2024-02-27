059592c7ddb0f69872e63b2b376bb217ea5ecd6f
Richard Avenia <90197861+rilarchsen@users.noreply.github.com>
2024-02-27 18:17:48 +0100

    moved grade column

--- content.js
+++ content.js
@@ -22,9 +22,9 @@ const newRowHTML = `
   <tr class="DataSelect" title="Detaljer for resultatet">
     <td class="DataValue" valign="top"> <strong>Weighted Average:</strong></td>
     <td class="DataValue" align="center" valign="top"></td>
-    <td class="DataValue" valign="top" align="center"></td>
+    <td class="DataValue" valign="top" align="center"><strong>${average.toFixed(1)}&nbsp;&nbsp;</strong></td>
     <td class="DataValue" align="center" valign="top"></td>
-    <td class="DataValue" align="right" valign="top"><strong>${average.toFixed(1)}&nbsp;&nbsp;</strong></td>
+    <td class="DataValue" align="right" valign="top"></td>
   </tr>
 `;
 
