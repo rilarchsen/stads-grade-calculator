93f59e98b67a1265aed9ab4b48b01abcbe483df0
Richard Avenia <90197861+rilarchsen@users.noreply.github.com>
2024-02-27 17:04:15 +0100

    made chrome extension :)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # stads-grade-calculator
-A chrome extension to calculate and display the weighted average grade on STADS
+A Chrome extension to calculate and display the weighted average grade on STADS.
+
+## How to use:
+1. Clone repo.
+2. Open Google Chrome.
+3. Go to chrome://extensions/.
+4. Enable "Developer mode" by toggling the switch in the top right corner.
+5. Click on "Load unpacked" and select the repo directory.
+6. The extension should now be loaded and active in Chrome.
+7. If all else fails, get ChatGPT to debug it 😉.
---
+++ content.js
@@ -0,0 +1,31 @@
+let totalEcts = 0;
+let gradeSum = 0;
+
+const exam = document.querySelectorAll(".DataSelect");
+
+exam.forEach((elem) => {
+  const grade = parseFloat(elem.children[2].textContent);
+  const ects = parseFloat(elem.children[4].textContent);
+
+  if (!isNaN(grade)) {
+    gradeSum += grade * ects;
+    totalEcts += ects;
+  }
+});
+
+const average = gradeSum / totalEcts;
+
+const table = document.getElementById("resultTable");
+const tbody = table.getElementsByTagName("tbody")[0];
+
+const newRowHTML = `
+  <tr class="DataSelect" title="Detaljer for resultatet">
+    <td class="DataValue" valign="top"> <strong>Weighted Average:</strong></td>
+    <td class="DataValue" align="center" valign="top"></td>
+    <td class="DataValue" valign="top" align="center"></td>
+    <td class="DataValue" align="center" valign="top"></td>
+    <td class="DataValue" align="right" valign="top"><strong>${average.toFixed(1)}&nbsp;&nbsp;</strong></td>
+  </tr>
+`;
+
+tbody.innerHTML += newRowHTML;
